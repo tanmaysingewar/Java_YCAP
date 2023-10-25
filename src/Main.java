3e0dde38a04bb6f857b3120c46837698e93cf5e3
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2023-10-25 11:18:37 +0530

    Tep Casting

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int a = 10, b =20;
         // Output will be string
         System.out.println("Add="+a+b);
-        System.out.println(" ");
+        System.out.print("");
         // Output will be String and addition in int
         System.out.println("Add="+(a+b));
 
@@ -15,5 +15,10 @@ public class Main {
         //Tep Casting the variable
         short s = (short) a;
         System.out.println("Short="+s);
+
+//        Float
+        float f = (float)a;
+        System.out.println("Float="+f);
+
     }
 }
