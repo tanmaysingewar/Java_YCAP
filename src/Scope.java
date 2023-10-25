654e093bce2a1f0bf9b8fdd6a645f5ab52f80ba0
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2023-10-25 11:44:13 +0530

    Session End

--- src/Scope.java
+++ src/Scope.java
@@ -1,15 +1,17 @@
 public class Scope {
     public static void main(String[] args) {
-        // Loop
-//        int i =10;
+        // if Loop
+        // int i =10;
         for (int i = 0; i < 5; i++) {
             System.out.println("x="+i);
         }
 
-        for (int i = 0; i < 4 ; i++) {
+        for (int i = 1; i < 4 ; i++) {
             System.out.println("x="+i);
         }
-//        System.out.println(i);
-
+            // System.out.println(i);
+        for (;;) {
+            System.out.print("x");
+        }
     }
 }
