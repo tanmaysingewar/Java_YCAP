267127c162daaa96a7855d12120be907442122b3
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2023-10-25 16:08:59 +0530

    class and obj - last class

--- src/Obj.java
+++ src/Obj.java
@@ -1,17 +1,27 @@
 class Test {
     int x = 10;
     double d = 8.9;
-    
     public void show(){
         System.out.println("X=" + x);
         System.out.println("D=" + d);
     }
 }
 
+class Implement {
+    int A = 2;
+    int d = 7;
+    float result = A+ (float) (4 * d) /8;
+    public void show(){
+        System.out.println(result);
+    }
+}
+
 public class Obj {
     public static void main(String[] args) {
-        Test obj = new Test();
-
-        obj.show();
+//        Test obj1; // null obj
+        Test obj2 = new Test(); // memory allocation obj
+        obj2.show();
+        Implement imp = new Implement();
+        imp.show();
     }
 }
