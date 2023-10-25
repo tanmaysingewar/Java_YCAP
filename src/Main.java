13f0f56bb23e59bff816ca5bfa557ada76d20f11
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2023-10-25 11:06:50 +0530

    Corrected Program

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
         int a = 10, b =20;
         // Output will be string
         System.out.println("Add="+a+b);
+        System.out.println(" ");
         // Output will be String and addition in int
-        System.out.println("Add="+a+b);
+        System.out.println("Add="+(a+b));
+
+
 
     }
 }
