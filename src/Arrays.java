c1fd56abfbb8aab30d0af19eeb8095b7bd694669
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2023-10-26 16:04:54 +0530

    Arrays - fro loop

--- src/Arrays.java
+++ src/Arrays.java
@@ -3,8 +3,8 @@ public class Arrays {
         int[] a = {
              1,3,4,5,6,7
         };
-        for (int i =0; i<a.length;i++){
-            System.out.println(a[i]);
+        for (int j : a) {
+            System.out.println(j);
         }
         for (int i =0; i<5;i++){
             System.out.println(5+a[i]);
